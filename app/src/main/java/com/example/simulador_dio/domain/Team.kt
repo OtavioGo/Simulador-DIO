142600abe424ae3db6d25321f0db18cbbb1553fa
Otavio Gomes <otavio.gomestemporim@gmail.com>
2022-06-20 18:28:36 -0300

    Adicionando as Classes para o projeto

---
+++ app/src/main/java/com/example/simulador_dio/domain/Team.kt
@@ -0,0 +1,7 @@
+package com.example.simulador_dio.domain
+
+data class Team (
+    val name: String,
+    val stars: Int,
+    val image: String
+        )
--- app/src/main/java/com/example/simulador_dio/ui/DetailActivity.kt
+++ app/src/main/java/com/example/simulador_dio/ui/DetailActivity.kt
@@ -1,4 +1,4 @@
-package com.example.simulador_dio
+package com.example.simulador_dio.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
