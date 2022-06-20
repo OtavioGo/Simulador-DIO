df58537d7012ee9edee3cd49ae32f1940eec8200
Otavio Gomes <otavio.gomestemporim@gmail.com>
2022-06-20 10:16:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/simulador_dio/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.example.simulador_dio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.simulador_dio.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
