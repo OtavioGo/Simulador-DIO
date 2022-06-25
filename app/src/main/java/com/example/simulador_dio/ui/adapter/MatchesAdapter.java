b1be2703c6bd2f061f9a22d3aec23f98e54d6f84
Otavio Gomes <otavio.gomestemporim@gmail.com>
2022-06-25 12:31:00 -0300

    Adicionando o Firebase crashlytics.

--- app/src/main/java/com/example/simulador_dio/ui/adapter/MatchesAdapter.java
+++ app/src/main/java/com/example/simulador_dio/ui/adapter/MatchesAdapter.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class MatchesAdapter extends RecyclerView.Adapter<MatchesAdapter.ViewHolder>{
 
-    private List<Match> matches;
+    private final List<Match> matches;
 
     public MatchesAdapter(List<Match> matches) {
         this.matches = matches;
